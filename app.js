5d7bd089f78f62588d8efc45d41b08032fbf8c36
Adit Patil <adit_patil@yahoo.com>
2017-09-11 21:39:11 +0530

    Dice Function added

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0]
+roundScore = 0 ;
+activePlayer = 2;
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    
+   var dice = Math.floor(Math.random() * 6) + 1;
+   
+    var diceDOM = document.querySelector('.dice');
+   diceDOM.style.display = 'block';
+   diceDOM.src = 'dice-' + dice + '.png'; 
+   
+})
